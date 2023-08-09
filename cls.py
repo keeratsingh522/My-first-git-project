3f751f8e566210e49b989ac00582bc4687d2ab4f
Keerat <keeratsingh522@gmail.com>
2023-08-08 20:58:06 -0400

    Added more pies

--- cls.py
+++ cls.py
@@ -6,6 +6,21 @@ class B:
 
     def infor(self):
         return f"My name is {self.aname} {self.lname} \n I am {self.aage} years old."
+    
+class C(B):
+    def __init__(self,aname,lname,aage,agrade,apost ):
+        super().__init__(aname, lname, aage)
+        self.agrade = agrade
+        self.apost = apost
+    
+    def fullinfo(self):
+        return f"My name is {self.aname} {self.lname} \n I am {self.aage} years old .Gdare is {self.agrade} post is {self.apost}"
+    
+
 
 b = B("Keeart","Singh",37)
-print(b.infor())       
+# print(b.infor()) 
+
+c = C("Mukesh","Kaur",36,"4th Grade","Programer")
+
+print(c.fullinfo())
